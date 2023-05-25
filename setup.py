0a683a2b7403287549405bced475510805e6a594
Tomas Vitvar <tomas@vitvar.com>
2023-05-25 11:15:46 +0200

    scm

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @author: Tomas Vitvar, https://vitvar.com, tomas@vitvar.com
 
-from __future__ import absolute_import
-from __future__ import unicode_literals
-
 import codecs
 import os
-import re
-import sys
-import argparse
-import glob
 
 from setuptools import find_packages
 from setuptools import setup
@@ -23,7 +15,6 @@ def read(*parts):
         return fobj.read()
 
 
-# setup main
 # required modules
 install_requires = [
     "influxdb>=5.3.1",
@@ -32,16 +23,18 @@ install_requires = [
     "lxml>=4.9.2",
     "click>=8.1.3",
     "pyyaml>=6.0",
+    "setuptools_scm>=6.0.1",
 ]
 
 setup(
     name="yamc-server",
-    use_scm_version={"root": "..", "relative_to": __file__, "local_scheme": "node-and-timestamp"},
+    use_scm_version={"root": ".", "relative_to": __file__, "local_scheme": "node-and-timestamp"},
     description="Yet Another Metric Collector",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Tomas Vitvar",
     author_email="tomas@vitvar.com",
+    py_modules=["yamc"],
     packages=find_packages(exclude=["tests.*", "tests"]),
     include_package_data=True,
     install_requires=install_requires,
